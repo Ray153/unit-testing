4b5635b84d0238cdcbcca402124474a33068319d
husam-shakeeb <husam_shakeeb@hotmail.com>
2023-04-18 23:50:25 +0200

    comment resolution

--- src/Services/SimpleMath/SimpleMath.ts
+++ src/Services/SimpleMath/SimpleMath.ts
@@ -1,11 +1,4 @@
 export class MyMath {
-	num1: number
-	num2: number
-
-	constructor (num1: number, num2: number){
-		this.num1 = num1
-		this.num2 = num2
-	}
 
 	add(num1: number, num2: number): number{
 		return num1 + num2
@@ -17,7 +10,7 @@ export class MyMath {
 		return num1 * num2
 	}
 	divide(num1: number, num2: number): number{
-		if (num2==0){
+		if (num2 == 0){
 			console.log("Can't divide by 0")
 			return 0
 		}
--- src/__tests__/Calculator.test.ts
+++ src/__tests__/Calculator.test.ts
@@ -1,29 +1,29 @@
 import { Calculator } from "../Calculator/Calculator"
 
 describe("Calculator", () => {
-  let calculator: Calculator;
+  let calculator: Calculator
 
-  beforeEach(() => {
-    calculator = new Calculator();
-  });
+  beforeAll(() => {
+    calculator = new Calculator()
+  })
 
   test("should add two numbers correctly", () => {
-    expect(calculator.add(2, 3)).toEqual(5);
-  });
+    expect(calculator.add(2, 3)).toEqual(5)
+  })
 
   test("should subtract two numbers correctly", () => {
-    expect(calculator.subtract(5, 3)).toEqual(2);
-  });
+    expect(calculator.subtract(5, 3)).toEqual(2)
+  })
 
   test("should multiply two numbers correctly", () => {
-    expect(calculator.multiply(4, 3)).toEqual(12);
-  });
+    expect(calculator.multiply(4, 3)).toEqual(12)
+  })
 
   test("should divide two numbers correctly", () => {
-    expect(calculator.divide(10, 2)).toEqual(5);
-  });
+    expect(calculator.divide(10, 2)).toEqual(5)
+  })
 
   test("should throw error when dividing by zero", () => {
-    expect(() => calculator.divide(10, 0)).toThrowError("Cannot divide by zero");
-  });
-});
+    expect(() => calculator.divide(10, 0)).toThrowError("Cannot divide by zero")
+  })
+})
